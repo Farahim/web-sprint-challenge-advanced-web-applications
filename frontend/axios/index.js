ca6f933d21ae7fd7623e04082321279446e5f887
Farahim Ibrahimli <i.farahim@gmail.com>
2024-11-14 01:30:39 -0500

    done

---
+++ frontend/axios/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export default function axiosWithAuth() {
+
+    const token = localStorage.getItem('token');
+
+
+    return axios.create({
+        headers: {
+            Authorization: token,
+        },
+    });
+}
